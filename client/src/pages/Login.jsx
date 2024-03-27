28b6dd3826113141dfb67f20e218568e3e402dee
Akshat Biniwale <akshatbiniwale@gmail.com>
2024-03-27 22:20:47 +0530

    initial commit

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+const Login = () => {
+	return (
+		<section className="bg-[#F7F4F2] grid h-screen grid-cols-2">
+			<div className="grid-cols-2">sec1</div>
+			<div className="grid-cols-2">sec2</div>
+		</section>
+	);
+};
+
+export default Login;
